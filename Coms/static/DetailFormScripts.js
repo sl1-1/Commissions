c4bb325bc409d87f94e4ed55b2de10c03331fcc7
sl1 <sl1.1@outlook.com>
2015-12-04 20:26:47 -0500

    Fixed Contact validation/javascript from form rename

--- Coms/static/DetailFormScripts.js
+++ Coms/static/DetailFormScripts.js
@@ -3,21 +3,21 @@ function setContact() {
         $(this).val("False")
     });
     var formid = $('#PC').val();
-    $('#id_contact_set-' + formid + '-primary').val(true);
+    $('#id_form-' + formid + '-primary').val(true);
 }
 
 function getOption() {
-    var forms = $("#id_contact_set-TOTAL_FORMS").val();
+    var forms = $("#id_form-TOTAL_FORMS").val();
     var select = $('#PC');
     $("select[id$=PC] > option").remove();
     select.empty();
     select.append(new Option('Email', 999, false, false));
     for (var i = 0; i < forms; i++) {
-        var text = $('#id_contact_set-' + i + '-site :selected').text() + ': ' + $('#id_contact_set-' + i + '-username').val();
-        if (!$("#id_contact_set-" + i + "-DELETE").prop('checked')) {
-            if ($('#id_contact_set-' + i + '-site').val()) {
+        var text = $('#id_form-' + i + '-site :selected').text() + ': ' + $('#id_form-' + i + '-username').val();
+        if (!$("#id_form-" + i + "-DELETE").prop('checked')) {
+            if ($('#id_form-' + i + '-site').val()) {
                 select.append(new Option(text, i, false, false));
-                if ($('#id_contact_set-' + i + '-primary').val().toLowerCase() == "true") {
+                if ($('#id_form-' + i + '-primary').val().toLowerCase() == "true") {
                     select.find('option:last').prop('selected', 'selected');
                 }
             }
