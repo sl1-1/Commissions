adc6e9c1a9556ae484f2031c3a1172e1e4f5522d
sl1 <sl1.1@outlook.com>
2016-10-15 20:27:19 -0400

    Moved Nav out to it's own directive Added loading notification

--- Angular/scripts/main.js
+++ Angular/scripts/main.js
@@ -76,26 +76,3 @@ app.controller('MainCtrl',
         MainCtrl
     ]
 );
-
-function NavCtrl($scope, $state, UserData, loginModalService) {
-    $scope.user = UserData;
-
-    $scope.login = function() {
-        loginModalService();
-    };
-
-    $scope.logout = function() {
-        $state.go('index');
-        UserData.logout();
-    };
-}
-
-app.controller('NavCtrl',
-    [
-        '$scope',
-        '$state',
-        'UserData',
-        'loginModalService',
-        NavCtrl
-    ]
-);
